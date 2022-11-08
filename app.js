79b22acc372c79755bd912cde356fea0d877b191
Matiaslopezp <lopezp.matias@gmail.com>
2022-11-08 01:42:35 +0000

    tercer save

--- app.js
+++ app.js
@@ -1,2 +1,44 @@
-// app.js file content
-console.log("Hello World")
+// esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// solo un registro en consola para nosotros.
+console.log(sum(7,3))
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+module.exports = { sum };
+
+// one euro is:
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+function fromEuroToDollar(euro){
+    let dollar=oneEuroIs.USD*euro;
+    return dollar;
+}
+
+function fromDollarToYen(dollar){
+ let yen=(oneEuroIs.JPY/(1/oneEuroIs.USD))*dollar;
+ return yen;
+}
+
+
+function fromYenToPound(yen){
+    let pound=(oneEuroIs.GBP/(1/oneEuroIs.JPY))*yen;
+    return pound;
+   }
+   
+
+
+console.log(fromEuroToDollar(1));
+console.log(fromDollarToYen(1));
+console.log(fromYenToPound(1));
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+module.exports = { sum, fromEuroToDollar };
---
+++ test.js
@@ -0,0 +1,7 @@
+test("One euro should be 1.206 dollars", function(){
+    // importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+})
